b2265a33380508a4abfdd2b660548610cb79248d
Eric Fleming <eric-fleming18@hotmail.com>
2013-10-04 23:39:45 -0400

    A little cleanup

--- src/GildedRose.Tests/UpdateQualityTests.cs
+++ src/GildedRose.Tests/UpdateQualityTests.cs
@@ -32,7 +32,22 @@ namespace GildedRose.Tests
         [Test]
         public void GivenOneOfEachItemType_WhenQualityIs0_NeverReturnQualityUnder0()
         {
-            Assert.True(false);
+            var app = new GildedRose.Console.Program();
+            var quality0Items = new List<Item> 
+            {
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0},
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 0},
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 0},
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 0 },
+                new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 0}
+            };
+            app.Items = quality0Items;
+
+            app.UpdateQuality();
+            var itemsWithQualityUnder0 = app.Items.Where(i => i.Quality >= 0);
+
+            CollectionAssert.IsEmpty(itemsWithQualityUnder0);
         }
     }
 }
