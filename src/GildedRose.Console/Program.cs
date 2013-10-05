57c04fc4b776a9ab80a90ec531d2fee8ce9ec6f5
Eric Fleming <eric-fleming18@hotmail.com>
2013-10-05 00:18:44 -0400

    Green - Never return items with negative quality

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -17,7 +17,7 @@ namespace GildedRose.Console
                                   new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                                   new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                                   new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                  new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                                  new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
                                   new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
                               }
                           };
--- src/GildedRose.Tests/UpdateQualityTests.cs
+++ src/GildedRose.Tests/UpdateQualityTests.cs
@@ -30,24 +30,24 @@ namespace GildedRose.Tests
         }
 
         [Test]
-        public void GivenOneOfEachItemType_WhenQualityIs0_NeverReturnQualityUnder0()
+        public void GivenOneOfEachItemType_WhenQualityIsNegative_NeverReturnItem()
         {
             var app = new GildedRose.Console.Program();
             var quality0Items = new List<Item> 
             {
-                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0},
-                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 0},
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 0},
-                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 0 },
-                new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 0}
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = -1},
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = -2},
+                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = -3},
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = -4},
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = -5},
+                new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = -6}
             };
             app.Items = quality0Items;
 
             app.UpdateQuality();
-            var itemsWithQualityUnder0 = app.Items.Where(i => i.Quality >= 0);
+            var itemsWithQualityOf0OrGreater = app.Items.Where(i => i.Quality >= 0);
 
-            CollectionAssert.IsEmpty(itemsWithQualityUnder0);
+            CollectionAssert.IsEmpty(itemsWithQualityOf0OrGreater);
         }
     }
 }
