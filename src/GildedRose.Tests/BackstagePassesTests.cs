bdebbe110d66b7d9e79efcaae6e6f718c9db71ff
Rich Hildebrand <richhildebrand@gmail.com>
2013-10-06 22:26:34 -0400

    Red - Another possible path

---
+++ src/GildedRose.Tests/BackstagePassesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    class BackstagePassesTests : BaseItemTests
+    {
+        protected override string ItemToTest { get { return "Backstage passes to a TAFKAL80ETC concert"; } }
+    }
+}
---
+++ src/GildedRose.Tests/MongooseElixirTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    class MongooseElixirTests : BaseItemTests
+    {
+        protected override string ItemToTest { get { return "Elixir of the Mongoose"; } }
+    }
+}
